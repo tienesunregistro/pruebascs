60b0fa2229b51b78c6f83448412c6e3d23317d7b
Juan Jose <tienesunregistro@gmail.com>
2017-02-23 01:31:09 +0100

    segundo commit

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "ejemplo1";
+            label1.Text = "ejemplo1 ej3 ssss";
         }
     }
 }
